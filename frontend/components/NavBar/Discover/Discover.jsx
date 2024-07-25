1cf163dbdcbea71d0b2d842dc7380d98a60b0a62
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-07-25 21:05:32 +0800

    dropdown menus

--- frontend/components/NavBar/Discover/Discover.jsx
+++ frontend/components/NavBar/Discover/Discover.jsx
@@ -1,8 +1,48 @@
 import Style from './Discover.module.css';
+import Link from 'next/link';
 
 const Discover = () => {
+  const discover = [
+    {
+      name: "Collection",
+      link: "collection"
+    },
+    {
+      name: "Search",
+      link: "search"
+    },
+    {
+      name: "Author Profile",
+      link: "author-profile"
+    },
+    {
+      name: "NFT Details",
+      link: "NFT-details"
+    },
+    {
+      name: "Account Setting",
+      link: "account-setting"
+    },
+    {
+      name: "Connect Wallet",
+      link: "connect-wallet"
+    },
+    {
+      name: "Blog",
+      link: "blog"
+    },
+  ]
+
   return (
-    <div>Discover</div>
+    <div>
+      {discover.map((el, i) => (
+        <div key = {i + 1} className={Style.discover}>
+          <Link href={{ pathname: `${el.link}` }}>
+            {el.name}
+          </Link>
+        </div>  
+      ))}
+    </div>
   );
 }
 
